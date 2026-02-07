095c5b8f7cf69d1b971a9e51006adda25eb521e3
orxfun <orx.ugur.arikan@gmail.com>
2026-02-07 20:16:19 +0100

    instantiate Experiment trait

--- src/experiment.rs
+++ src/experiment.rs
@@ -9,6 +9,10 @@ pub trait Experiment<const T: usize, const V: usize> {
 
     type Output;
 
+    fn execution_to_string(treatment: &Self::Treatment, variant: &Self::Variant) -> String {
+        format!("{}__{}", treatment.to_string(), variant.to_string())
+    }
+
     fn input(treatment: &Self::Treatment) -> Self::Input;
 
     fn execute(variant: &Self::Variant, input: &Self::Input) -> Self::Output;
--- src/tests/experiment.rs
+++ src/tests/experiment.rs
@@ -12,32 +12,27 @@ impl Treatment<1> for MyTreat1 {
     }
 }
 
-pub struct MyVariant3 {
+pub struct MyVariant2 {
     len: usize,
     sort: bool,
-    split: char,
 }
 
-impl Variant<3> for MyVariant3 {
-    fn param_names() -> [&'static str; 3] {
-        ["len", "sort", "split"]
+impl Variant<2> for MyVariant2 {
+    fn param_names() -> [&'static str; 2] {
+        ["len", "sort"]
     }
 
-    fn param_values(&self) -> [String; 3] {
-        [
-            self.len.to_string(),
-            self.sort.to_string(),
-            self.split.to_string(),
-        ]
+    fn param_values(&self) -> [String; 2] {
+        [self.len.to_string(), self.sort.to_string()]
     }
 }
 
 pub struct MyExperiment;
 
-impl Experiment<1, 3> for MyExperiment {
+impl Experiment<1, 2> for MyExperiment {
     type Treatment = MyTreat1;
 
-    type Variant = MyVariant3;
+    type Variant = MyVariant2;
 
     type Input = Vec<usize>;
 
@@ -50,10 +45,57 @@ impl Experiment<1, 3> for MyExperiment {
     fn execute(variant: &Self::Variant, input: &Self::Input) -> Self::Output {
         let mut output = input.clone();
         if variant.sort {
-            for i in 1..variant.len {
-                output.swap(i, i - 1);
+            for _ in 0..variant.len {
+                for i in 1..output.len() {
+                    output.swap(i, i - 1);
+                }
             }
         }
         output
     }
+}
+
+#[test]
+fn basic_experiment() {
+    let treatments = vec![MyTreat1(2), MyTreat1(5)];
+    let variants = vec![
+        MyVariant2 {
+            len: 1001,
+            sort: false,
+        },
+        MyVariant2 {
+            len: 1001,
+            sort: true,
+        },
+    ];
+
+    let mut outputs = vec![];
+    let mut names = vec![];
+    for treatment in &treatments {
+        let input = MyExperiment::input(treatment);
+        for variant in &variants {
+            names.push(MyExperiment::execution_to_string(treatment, variant));
+            outputs.push(MyExperiment::execute(variant, &input));
+        }
+    }
+
+    assert_eq!(
+        outputs,
+        vec![
+            vec![0, 1],
+            vec![1, 0],
+            vec![0, 1, 2, 3, 4],
+            vec![1, 2, 3, 4, 0],
+        ]
+    );
+
+    assert_eq!(
+        names,
+        vec![
+            "width:2__len:1001_sort:false",
+            "width:2__len:1001_sort:true",
+            "width:5__len:1001_sort:false",
+            "width:5__len:1001_sort:true"
+        ]
+    );
 }
