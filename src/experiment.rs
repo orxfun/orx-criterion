aeb205efb790de32cb3d27c1d14be28f22b88565
orxfun <orx.ugur.arikan@gmail.com>
2026-02-07 20:23:54 +0100

    implement Experiment::bench

--- src/experiment.rs
+++ src/experiment.rs
@@ -1,4 +1,5 @@
 use crate::{Treatment, Variant};
+use criterion::Criterion;
 
 pub trait Experiment<const T: usize, const V: usize> {
     type Treatment: Treatment<T>;
@@ -16,4 +17,27 @@ pub trait Experiment<const T: usize, const V: usize> {
     fn input(treatment: &Self::Treatment) -> Self::Input;
 
     fn execute(variant: &Self::Variant, input: &Self::Input) -> Self::Output;
+
+    fn bench(
+        c: &mut Criterion,
+        name: &str,
+        treatments: &[Self::Treatment],
+        variants: &[Self::Variant],
+    ) {
+        let mut group = c.benchmark_group(name);
+
+        for treatment in treatments {
+            let input = Self::input(treatment);
+            for variant in variants {
+                let execution_name = Self::execution_to_string(treatment, variant);
+
+                group.bench_with_input(execution_name, &input, |b, input| {
+                    // let output = Self::execute(variant, input);
+                    b.iter(|| Self::execute(variant, input));
+                });
+            }
+        }
+
+        group.finish();
+    }
 }
