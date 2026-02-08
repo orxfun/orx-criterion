b98618304458769b6a1cad00bed0c1051affacda
orxfun <orx.ugur.arikan@gmail.com>
2026-02-08 07:45:50 +0100

    remove const generics from Variant

--- src/variant.rs
+++ src/variant.rs
@@ -1,9 +1,9 @@
 use crate::treatment::to_string;
 
-pub trait Variant<const N: usize> {
-    fn param_names() -> [&'static str; N];
+pub trait Variant {
+    fn param_names() -> Vec<&'static str>;
 
-    fn param_values(&self) -> [String; N];
+    fn param_values(&self) -> Vec<String>;
 
     fn to_string(&self) -> String {
         to_string(&Self::param_names(), &self.param_values())
