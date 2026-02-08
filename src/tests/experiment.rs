ec3a1929da12fa478ecac0dd567e0fec4a81757d
orxfun <orx.ugur.arikan@gmail.com>
2026-02-08 15:23:40 +0100

    add short name tests to Experiment

--- src/tests/experiment.rs
+++ src/tests/experiment.rs
@@ -7,6 +7,10 @@ impl Treatment for MyTreat1 {
         vec!["width"]
     }
 
+    fn factor_names_short() -> Vec<&'static str> {
+        vec!["w"]
+    }
+
     fn factor_values(&self) -> Vec<String> {
         vec![self.0.to_string()]
     }
@@ -22,8 +26,23 @@ impl Variant for MyVariant2 {
         vec!["len", "sort"]
     }
 
+    fn param_names_short() -> Vec<&'static str> {
+        vec!["l", "s"]
+    }
+
     fn param_values(&self) -> Vec<String> {
         vec![self.len.to_string(), self.sort.to_string()]
+    }
+
+    fn param_values_short(&self) -> Vec<String> {
+        vec![
+            self.len.to_string(),
+            match self.sort {
+                true => "T",
+                false => "F",
+            }
+            .to_string(),
+        ]
     }
 }
 
@@ -71,10 +90,12 @@ fn basic_experiment() {
 
     let mut outputs = vec![];
     let mut names = vec![];
+    let mut names_short = vec![];
     for treatment in &treatments {
         let input = MyExperiment::input(treatment);
         for variant in &variants {
             names.push(MyExperiment::run_key_long(treatment, variant));
+            names_short.push(MyExperiment::run_key_short(treatment, variant));
             outputs.push(MyExperiment::execute(variant, &input));
         }
     }
@@ -98,49 +119,14 @@ fn basic_experiment() {
             "width:5/len:1001_sort:true"
         ]
     );
-}
-
-#[test]
-fn experiment_bench() {
-    let treatments = [MyTreat1(2), MyTreat1(5)];
-    let variants = [
-        MyVariant2 {
-            len: 1001,
-            sort: false,
-        },
-        MyVariant2 {
-            len: 1001,
-            sort: true,
-        },
-    ];
-
-    let mut outputs = vec![];
-    let mut names = vec![];
-    for treatment in &treatments {
-        let input = MyExperiment::input(treatment);
-        for variant in &variants {
-            names.push(MyExperiment::run_key_long(treatment, variant));
-            outputs.push(MyExperiment::execute(variant, &input));
-        }
-    }
 
     assert_eq!(
-        outputs,
+        names_short,
         [
-            vec![0, 1],
-            vec![1, 0],
-            vec![0, 1, 2, 3, 4],
-            vec![1, 2, 3, 4, 0],
-        ]
-    );
-
-    assert_eq!(
-        names,
-        [
-            "width:2/len:1001_sort:false",
-            "width:2/len:1001_sort:true",
-            "width:5/len:1001_sort:false",
-            "width:5/len:1001_sort:true"
+            "w:2/l:1001_s:F",
+            "w:2/l:1001_s:T",
+            "w:5/l:1001_s:F",
+            "w:5/l:1001_s:T"
         ]
     );
 }
