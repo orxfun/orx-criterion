b37551d67d4c27e01354d97e990e670c868d69fe
orxfun <orx.ugur.arikan@gmail.com>
2026-02-10 10:06:23 +0100

    setup benchmark

--- benches/tuning_example.rs
+++ benches/tuning_example.rs
@@ -3,6 +3,46 @@ use orx_criterion::{Data, Experiment, Variant};
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 
+// Variant
+
+#[derive(Debug)]
+enum Direction {
+    Forwards,
+    Backwards,
+}
+
+struct AlgParams {
+    num_threads: usize,
+    direction: Direction,
+}
+
+impl Variant for AlgParams {
+    fn param_names() -> Vec<&'static str> {
+        vec!["num_threads", "direction"]
+    }
+
+    fn param_values(&self) -> Vec<String> {
+        vec![
+            self.num_threads.to_string(),
+            format!("{:?}", self.direction),
+        ]
+    }
+
+    fn param_names_short() -> Vec<&'static str> {
+        vec!["n", "d"]
+    }
+
+    fn param_values_short(&self) -> Vec<String> {
+        let direction = match self.direction {
+            Direction::Forwards => "F",
+            Direction::Backwards => "B",
+        };
+        vec![self.num_threads.to_string(), direction.to_string()]
+    }
+}
+
+// Instance
+
 fn run(c: &mut Criterion) {
     // let data = [
     //     DataSettings(1 << 5),
