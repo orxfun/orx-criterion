49d152628f6ab161e37126f70b7f80482e75bd74
orxfun <orx.ugur.arikan@gmail.com>
2026-02-10 11:03:56 +0100

    extend tuning benchmark example

--- benches/tuning_example.rs
+++ benches/tuning_example.rs
@@ -1,5 +1,6 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use orx_criterion::{AlgFactors, Experiment, InputFactors};
+use orx_parallel::{ParIter, ParallelizableCollection};
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 use rayon::iter::Positions;
@@ -49,6 +50,7 @@ enum ValuePosition {
     Beg,
     Mid,
     End,
+    None,
 }
 
 struct Settings {
@@ -74,8 +76,73 @@ impl InputFactors for Settings {
             ValuePosition::Beg => "B",
             ValuePosition::Mid => "M",
             ValuePosition::End => "E",
+            ValuePosition::None => "X",
         };
         vec![self.len.to_string(), position.to_string()]
+    }
+}
+
+// Experiment
+
+const SEARCH_VALUE: &str = "criterion";
+
+struct Input {
+    array: Vec<String>,
+    exists: bool,
+}
+
+struct SearchExp;
+
+impl Experiment for SearchExp {
+    type Data = Settings;
+
+    type Variant = Params;
+
+    type Input = Input;
+
+    type Output = bool;
+
+    fn input(data: &Self::Data) -> Self::Input {
+        let mut array: Vec<_> = (0..data.len).map(|i| i.to_string()).collect();
+        let position = match data.position {
+            ValuePosition::Beg => data.len / 5,
+            ValuePosition::Mid => data.len / 2,
+            ValuePosition::End => 4 * data.len / 5,
+            ValuePosition::None => data.len,
+        };
+        // we place the search value at the position
+        if let Some(element) = array.get_mut(position) {
+            *element = SEARCH_VALUE.to_string();
+        }
+        let exists = position < array.len();
+        Input { array, exists }
+    }
+
+    fn execute(alg_variant: &Self::Variant, input: &Self::Input) -> Self::Output {
+        match alg_variant.direction {
+            Direction::Forwards => input
+                .array
+                .par()
+                .num_threads(alg_variant.num_threads)
+                .find(|x| x.as_str() == SEARCH_VALUE)
+                .is_some(),
+            Direction::Backwards => {
+                let chunk_size = input.array.len() / alg_variant.num_threads;
+                let chunks: Vec<_> = input.array.chunks(chunk_size).collect();
+                std::thread::scope(|s| {
+                    let mut handles = vec![];
+                    for chunk in chunks {
+                        handles.push(s.spawn(|| {
+                            chunk
+                                .iter()
+                                .position(|x| x.as_str() == SEARCH_VALUE)
+                                .is_some()
+                        }));
+                    }
+                    handles.into_iter().map(|h| h.join().unwrap()).any(|x| x)
+                })
+            }
+        }
     }
 }
 
@@ -91,7 +158,6 @@ fn run(c: &mut Criterion) {
     //     SearchMethod(StoreType::HashMap),
     //     SearchMethod(StoreType::BTreeMap),
     // ];
-
     // TwoSumExp::bench(c, "two_sum", &data, &variants);
 }
 
